d61f5e946351abf6f0f1c42947f70821414467b5
Jackson Bates <jkbates222@gmail.com>
2018-09-19 16:46:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var arr = {};
+  for (let i = 0; i < musicians.length; i++) {
+    arr = [...arr, musicians[i] + " plays " + instruments[i]];
+  }
+  return arr;
+}
+var num = 0;
+var johnLennonFacts = function(facts) {
+  while (num < facts.length) {
+    var thing = facts;
+    thing.splice(num, 1, facts[num] + "!!!");
+    num++;
+    }
+  return thing;
+}
